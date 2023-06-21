85864a206ecd88925de75fda1054005f36d1b8fe
Deimian Vásquez <deimianvasquez@gmail.com>
2023-06-21 19:51:56 -0400

    add models and migrations

--- src/api/models.py
+++ src/api/models.py
@@ -2,11 +2,19 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    last_name = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(180), unique=False, nullable=False)
+    salt = db.Column(db.String(100), unique=False, nullable=False)
+    avata = db.Column(db.String(100), unique=False, nullable=False)
+    create_at = db.Column(db.DateTime, nullable=False,
+                          default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, nullable=False, onupdate=db.func.current_timestamp(
+    ), default=db.func.current_timestamp())
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -16,4 +24,25 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(255), unique=False, nullable=False)
+    done = db.Column(db.Boolean, unique=False, nullable=False)
+    create_at = db.Column(db.DateTime, nullable=False,
+                          default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, nullable=False, onupdate=db.func.current_timestamp(
+    ), default=db.func.current_timestamp())
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "label": self.label,
         }
