c8df3bef0571c8bf84879498f86aa96a2a0b1863
Deimian Vásquez <deimianvasquez@gmail.com>
2023-06-21 20:15:28 -0400

    add login and register

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,15 +4,82 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from base64 import b64encode
+import os
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+def set_password(password, salt):
+    return generate_password_hash(f"{password}{salt}")
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
 
-    return jsonify(response_body), 200
+def check_password(hash_password, password, salt):
+    return check_password_hash(hash_password, f"{password}{salt}")
+
+
+@api.route('/user', methods=['POST'])
+def register_user():
+    if request.method == "POST":
+        data = request.json
+
+        if data is None:
+            return jsonify({"msg": "Missing JSON in request"}), 400
+        if data.get("name") is None:
+            return jsonify({"msg": "Missing name parameter"}), 400
+        if data.get("last_name") is None:
+            return jsonify({"msg": "Missing last name parameter"}), 400
+        if data.get("email") is None:
+            return jsonify({"msg": "Missing email parameter"}), 400
+        if data.get("password") is None:
+            return jsonify({"msg": "Missing password parameter"}), 400
+        if data.get("avatar") is None:
+            return jsonify({"msg": "Missing avatar parameter"}), 400
+
+        user = User.query.filter_by(email=data.get("email")).first()
+        if user is not None:
+            return jsonify({"msg": "Email already registered"}), 400
+
+        password_salt = b64encode(os.urandom(32)).decode('utf-8')
+        password_hash = set_password(data.get("password"), password_salt)
+
+        new_user = User(
+            name=data.get("name"),
+            last_name=data.get("last_name"),
+            email=data.get("email"),
+            password=password_hash,
+            avata=data.get("avatar"),
+            salt=password_salt
+        )
+
+        db.session.add(new_user)
+        try:
+            db.session.commit()
+            return jsonify({"msg": "User successfully registered"}), 201
+        except Exception as error:
+            db.session.rollback()
+            return jsonify({"msg": "Error registering user", "error": str(error)}), 500
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    if request.method == "POST":
+        data = request.json
+        email = data.get("email", None)
+        password = data.get("password", None)
+
+        if email is None:
+            return jsonify({"msg": "Missing email parameter"}), 400
+        if password is None:
+            return jsonify({"msg": "Missing password parameter"}), 400
+
+        user = User.query.filter_by(email=email).one_or_none()
+        if user is not None:
+            if check_password(user.password, password, user.salt):
+                token = create_access_token(identity=user.id)
+                return jsonify({"token": token}), 200
+            else:
+                return jsonify({"msg": "Bad credentials"}), 400
